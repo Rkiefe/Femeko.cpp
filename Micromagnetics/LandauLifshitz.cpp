2d507260715acfd5e7ffb9a7d1557c7a74a873db
R. Kiefe <rodrigokiefe@ua.pt>
2025-06-20 11:27:45 +0100

    Add files via upload
    Migrating from Femeko.jl

---
+++ Micromagnetics/LandauLifshitz.cpp
@@ -0,0 +1,25 @@
+/*
+	Landau Lifshitz equation solver
+*/ 
+	
+#include "../src/BEMc.cpp"
+
+void LandauLifshitz()
+{
+	double mu0 = pi*4e-7;
+
+	
+
+}
+
+
+
+
+
+
+// For testing
+int main(void)
+{
+	printf("Hello world\n");
+	return 0;
+}
